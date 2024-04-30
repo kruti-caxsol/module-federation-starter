b5658256d719cc17d5d7e683def7b3a966f4b67b
kruti-caxsol <kruti.paneri@caxsol.com>
2024-04-30 23:16:14 +0530

    made codegen with graphql schema

--- services/codegen.ts
+++ services/codegen.ts
@@ -1,52 +1,15 @@
-// import { CodegenConfig } from "@graphql-codegen/cli";
-
-// const config: CodegenConfig = {
-//   schema: [
-//     {
-//       "http://localhost:8090/graphql": {
-//         headers: {},
-//       },
-//     },
-//   ],
-//   ignoreNoDocuments: true,
-//   generates: {
-//     "./src/generated/graphql.tsx": {
-//       documents: ["src/**/*.tsx", "src/**/*.ts"],
-//       plugins: [
-//         "typescript",
-//         "typescript-operations",
-//         "typescript-react-apollo",
-//       ],
-//       config: {
-//         withHooks: true,
-//       },
-//     },
-//   },
-// };
-
-// export default config;
-
-import { CodegenConfig } from "@graphql-codegen/cli";
+import { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
-  schema: [
-    {
-      "http://localhost:8090/graphql": {
-        headers: {},
-      },
+    overwrite: true,
+    schema: "http://localhost:8090/graphql",
+    documents: "src/graphql/**/*.{gql,graphql}",
+    generates: {
+        "./src/gql/": {
+            schema: './schema.graphql',
+            plugins: ['typescript', 'typescript-operations'],
+        },
     },
-  ],
-  ignoreNoDocuments: true,
-  generates: {
-    "./src/gql/": {
-      documents: ["src/**/*.tsx", "src/**/*.ts"],
-      preset: "client",
-      plugins: [],
-      config: {
-        withHooks: true,
-      },
-    },
-  },
-};
+}
 
-export default config;
+export default config
